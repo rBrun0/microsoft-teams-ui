935c397708e8cc9641b8b3bc7d8c4dbfcdcc33be
rBrun0 <romerobruno788@gmail.com>
2024-08-11 23:48:31 -0300

    first commit

--- src/components/mainAside/MainAside.tsx
+++ src/components/mainAside/MainAside.tsx
@@ -12,10 +12,10 @@ const MainAside = () => {
   return (
     <aside className="h-svh w-16 bg-neutral-950 pt-3">
           <ul className=" text-gray-500 space-y-2">
-            <li className="flex flex-col items-center text-[10px] cursor-pointer group hover:text-blue-500"><Link href={"#"}> <FaRegBell className="text-xl  text-gray-500 group-hover:text-blue-500"/> Atividade </Link></li>
-            <li className="flex flex-col items-center text-[10px] cursor-pointer group  hover:text-blue-500 "><ActiveLink href={"/comunity"}> <FaPeopleGroup className="text-xl text-gray-500  group-hover:text-blue-500"/> Comunidade </ActiveLink> </li>
+            <li className="flex flex-col items-center text-[10px] cursor-pointer group hover:text-blue-500"><Link href={"#"}> <FaRegBell className="text-xl  text-gray-500 group-hover:text-blue-500 translate-x-2"/> Atividade </Link></li>
+            <li className="flex flex-col items-center text-[10px] cursor-pointer group  hover:text-blue-500 "><ActiveLink href={"/comunity"}> <FaPeopleGroup className="text-xl text-gray-500  group-hover:text-blue-500 translate-x-5"/> Comunidade </ActiveLink> </li>
             <li className="flex flex-col items-center text-[10px] cursor-pointer group  hover:text-blue-500"><ActiveLink href={"/"}> <IoChatbubbleEllipsesOutline className="text-xl group-text-gray-500  hover:text-blue-500"/> Chat </ActiveLink> </li>
-            <li className="flex flex-col items-center text-[10px] cursor-pointer group  hover:text-blue-500"><Link href={"#"}> <CiCalendar className="text-xl text-gray-500  group-hover:text-blue-500"/> Calendario </Link></li>
+            <li className="flex flex-col items-center text-[10px] cursor-pointer group  hover:text-blue-500"><Link href={"#"}> <CiCalendar className="text-xl text-gray-500  group-hover:text-blue-500 translate-x-2"/> Calendario </Link></li>
           </ul>
         </aside>
   )
